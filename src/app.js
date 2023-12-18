fcf78d16e3f4bea18b9c4b080c66be111367c24e
One Choiz <onechoizcode@gmail.com>
2023-12-18 15:48:12 +0800

    added lists and tuples

--- src/app.js
+++ src/app.js
@@ -1,19 +1,19 @@
-// numbers like hex, decimal, binary, octal are all : number 
+// numbers like hex, decimal, binary, octal are all : number
 var decimal = 0.7;
 var hexNum = 0xf00f;
 var age = 10;
-// string is of type string 
+// string is of type string
 var userName = "John";
 // bools ar of type boolean
 var isEmployed = false;
 var isStudent = true;
-// undefined 
+// undefined
 var user = undefined;
 var n = null;
-// arrays differ depending on what values they  include 
+// arrays differ depending on what values they  include
 var fruits = ["banana", "strawberry", "apple"];
 var fruits2 = ["banana", "strawberry", "apple"];
-var arr = [34, 'hello', { name: "john" }, [1, 2, 3]];
+var arr = [34, "hello", { name: "john" }, [1, 2, 3]];
 arr[1] = "Hello World!";
 console.log(arr);
 var years = [2012, 2022, 2032];
@@ -24,12 +24,12 @@ var arr2 = [22, 44, 24, 33, 66, 77, 95853];
 var evenNumbers = arr2.filter(function (n) { return n % 2 === 0; });
 var oddNums = arr2.filter(function (n) { return n % 2 !== 0; });
 console.log("even numbers: ".concat(evenNumbers, ", odd  numbers are : ").concat(oddNums));
-// tuples  are like arrays with differrent fixed types  having their indices fixed  
-var abc = [6, 'hello'];
+// tuples  are like arrays with differrent fixed types  having their indices fixed
+var abc = [6, "hello"];
 var b = [true, "hello", 77];
-// can also have type inference , so you it is inferred 
+// can also have type inference , so you it is inferred
 var myTuple = ["Macos", 2022, true, "Ubuntu", false];
-// deconstruct a tuple 
+// deconstruct a tuple
 var coordinates = [10, 20];
 var x = coordinates[0], y = coordinates[1];
 console.log(x); // 10
@@ -40,3 +40,57 @@ var getUser = function () {
 };
 var user1 = getUser();
 console.log("name is ".concat(user1[0], ",  age is ").concat(user1[1]));
+// type enums
+// Enums provide a  way to work with sets of related constants here we have colors 
+var Colors;
+(function (Colors) {
+    Colors[Colors["Blue"] = 0] = "Blue";
+    Colors[Colors["Purple"] = 1] = "Purple";
+    Colors[Colors["Silver"] = 2] = "Silver";
+    Colors[Colors["Gold"] = 10] = "Gold";
+})(Colors || (Colors = {}));
+// getting the index from Keys 
+console.log(Colors.Blue);
+// getting the key using index
+console.log(Colors[10]);
+// practical use  of getting server res status 
+var HttpStatusCode;
+(function (HttpStatusCode) {
+    HttpStatusCode[HttpStatusCode["Ok"] = 200] = "Ok";
+    HttpStatusCode[HttpStatusCode["NotFound"] = 404] = "NotFound";
+    HttpStatusCode[HttpStatusCode["IntrnalServerError"] = 500] = "IntrnalServerError";
+})(HttpStatusCode || (HttpStatusCode = {}));
+var handleHttpResp = function (statusCode) {
+    if (statusCode === HttpStatusCode.Ok) {
+        console.log("request seccess");
+    }
+    else if (statusCode === HttpStatusCode.NotFound) {
+        console.log("not found");
+    }
+    else if (statusCode === HttpStatusCode.IntrnalServerError) {
+        console.log("Server side error");
+    }
+    else {
+        console.log("idk , some other  status code not handled here ");
+    }
+};
+// if you just pass a numbr will result in type error 
+handleHttpResp(HttpStatusCode.Ok);
+var handleHttpRespNumber = function (statusCode) {
+    if (statusCode === HttpStatusCode.Ok) {
+        console.log("request seccess");
+    }
+    else if (statusCode === HttpStatusCode.NotFound) {
+        console.log("not found");
+    }
+    else if (statusCode === HttpStatusCode.IntrnalServerError) {
+        console.log("Server side error");
+    }
+    else {
+        console.log("idk , some other  status code not handled here ");
+    }
+};
+handleHttpRespNumber(300); //idk , some other  status code not handled here
+handleHttpRespNumber(HttpStatusCode.IntrnalServerError); //Server side error
+// in js it will bw only compiled to var reqStaus = 0 /* Status.Success */;
+var reqStaus = "success" /* Status.Success */;
--- src/app.ts
+++ src/app.ts
@@ -1,40 +1,40 @@
-// numbers like hex, decimal, binary, octal are all : number 
-const decimal: number = 0.7
-const hexNum : number = 0xf00f
-const age : number = 10
-// string is of type string 
-const userName : string = "John"
+// numbers like hex, decimal, binary, octal are all : number
+const decimal: number = 0.7;
+const hexNum: number = 0xf00f;
+const age: number = 10;
+// string is of type string
+const userName: string = "John";
 // bools ar of type boolean
-const isEmployed: boolean = false
-const isStudent : boolean = true
+const isEmployed: boolean = false;
+const isStudent: boolean = true;
 
-// undefined 
-const user: undefined = undefined
-const  n: null = null
+// undefined
+const user: undefined = undefined;
+const n: null = null;
 
-// arrays differ depending on what values they  include 
-const fruits: Array <string> = ["banana", "strawberry", "apple"]
-const fruits2: string[] = ["banana", "strawberry", "apple"]
-let arr: any [] = [34, 'hello', {name:"john"},[1,2,3]]
-arr[1] = "Hello World!"
-console.log(arr)
-const years : number[] = [2012, 2022, 2032]
+// arrays differ depending on what values they  include
+const fruits: Array<string> = ["banana", "strawberry", "apple"];
+const fruits2: string[] = ["banana", "strawberry", "apple"];
+let arr: any[] = [34, "hello", { name: "john" }, [1, 2, 3]];
+arr[1] = "Hello World!";
+console.log(arr);
+const years: number[] = [2012, 2022, 2032];
 
 // mixed type arr
-let myMixedArr : (string | number)[] = ["33", 99]
+let myMixedArr: (string | number)[] = ["33", 99];
 
 // array methods
-let arr2 : number[] = [22,44,24,33,66,77,95853]
+let arr2: number[] = [22, 44, 24, 33, 66, 77, 95853];
 
-let evenNumbers: number []=  arr2.filter(n => n%2 === 0)
-let oddNums: number [] = arr2.filter(n => n%2 !== 0)
+let evenNumbers: number[] = arr2.filter((n) => n % 2 === 0);
+let oddNums: number[] = arr2.filter((n) => n % 2 !== 0);
 console.log(`even numbers: ${evenNumbers}, odd  numbers are : ${oddNums}`);
-// tuples  are like arrays with differrent fixed types  having their indices fixed  
-let  abc :[number , string ]= [6, 'hello']
-const b: [boolean, string , number] = [ true, "hello",77]
-// can also have type inference , so you it is inferred 
-const myTuple = ["Macos", 2022, true, "Ubuntu", false]
-// deconstruct a tuple 
+// tuples  are like arrays with differrent fixed types  having their indices fixed
+let abc: [number, string] = [6, "hello"];
+const b: [boolean, string, number] = [true, "hello", 77];
+// can also have type inference , so you it is inferred
+const myTuple = ["Macos", 2022, true, "Ubuntu", false];
+// deconstruct a tuple
 let coordinates: [number, number] = [10, 20];
 let [x, y] = coordinates;
 
@@ -43,11 +43,70 @@ console.log(y); // 20
 
 // Tuples are commonly used to represent the parameters and return types of functions:
 
-const getUser =(): [string , number]=>{
-   return ["JOhn DOe", 33]
-}
+const getUser = (): [string, number] => {
+  return ["JOhn DOe", 33];
+};
 
-const user1: [string, number] =getUser()
-
+const user1: [string, number] = getUser();
 
 console.log(`name is ${user1[0]},  age is ${user1[1]}`);
+
+// type enums
+// Enums provide a  way to work with sets of related constants here we have colors 
+enum Colors{ Blue, Purple, Silver, Gold = 10}
+// getting the index from Keys 
+console.log(Colors.Blue)
+// getting the key using index
+console.log(Colors[10])
+
+// practical use  of getting server res status 
+enum HttpStatusCode {
+    Ok = 200,
+    NotFound = 404,
+    IntrnalServerError = 500,
+
+    
+}
+
+const handleHttpResp = (statusCode: HttpStatusCode) : void =>{
+   if( statusCode === HttpStatusCode.Ok){
+    console.log("request seccess");
+   }else if (statusCode === HttpStatusCode.NotFound){
+    console.log("not found");
+   }else if(statusCode === HttpStatusCode.IntrnalServerError) {
+    console.log("Server side error");
+    
+   }
+   else{
+    console.log("idk , some other  status code not handled here ");
+   }
+}
+// if you just pass a numbr will result in type error 
+handleHttpResp(HttpStatusCode.Ok)
+
+const handleHttpRespNumber = (statusCode: number |  HttpStatusCode) : void =>{
+   if( statusCode === HttpStatusCode.Ok){
+    console.log("request seccess");
+   }else if (statusCode === HttpStatusCode.NotFound){
+    console.log("not found");
+   }else if(statusCode === HttpStatusCode.IntrnalServerError) {
+    console.log("Server side error");
+    
+   }
+   else{
+    console.log("idk , some other  status code not handled here ");
+   }
+}
+
+handleHttpRespNumber(300) //idk , some other  status code not handled here
+handleHttpRespNumber(HttpStatusCode.IntrnalServerError)//Server side error
+
+
+//  const modifier can be used  to create enums that will only generate  the value when used and won't complie automatically
+const enum Status {
+    Success = 'success',
+    Failure = 'failure'
+}
+// in js it will bw only compiled to var reqStaus = 0 /* Status.Success */; if not assigned to anything ,just - Success
+// and var reqStaus = "success" /* Status.Success */; if assigned like above -  Success = 'success',
+let reqStaus: Status = Status.Success 
