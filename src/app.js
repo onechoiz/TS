028309fc47adaf7d3c77091ce82a2cc56f2d0615
One Choiz <onechoizcode@gmail.com>
2023-12-18 14:08:01 +0800

    added list types

--- src/app.js
+++ src/app.js
@@ -1,2 +1,42 @@
-var x = 10;
-console.log(x);
+// numbers like hex, decimal, binary, octal are all : number 
+var decimal = 0.7;
+var hexNum = 0xf00f;
+var age = 10;
+// string is of type string 
+var userName = "John";
+// bools ar of type boolean
+var isEmployed = false;
+var isStudent = true;
+// undefined 
+var user = undefined;
+var n = null;
+// arrays differ depending on what values they  include 
+var fruits = ["banana", "strawberry", "apple"];
+var fruits2 = ["banana", "strawberry", "apple"];
+var arr = [34, 'hello', { name: "john" }, [1, 2, 3]];
+arr[1] = "Hello World!";
+console.log(arr);
+var years = [2012, 2022, 2032];
+// mixed type arr
+var myMixedArr = ["33", 99];
+// array methods
+var arr2 = [22, 44, 24, 33, 66, 77, 95853];
+var evenNumbers = arr2.filter(function (n) { return n % 2 === 0; });
+var oddNums = arr2.filter(function (n) { return n % 2 !== 0; });
+console.log("even numbers: ".concat(evenNumbers, ", odd  numbers are : ").concat(oddNums));
+// tuples  are like arrays with differrent fixed types  having their indices fixed  
+var abc = [6, 'hello'];
+var b = [true, "hello", 77];
+// can also have type inference , so you it is inferred 
+var myTuple = ["Macos", 2022, true, "Ubuntu", false];
+// deconstruct a tuple 
+var coordinates = [10, 20];
+var x = coordinates[0], y = coordinates[1];
+console.log(x); // 10
+console.log(y); // 20
+// Tuples are commonly used to represent the parameters and return types of functions:
+var getUser = function () {
+    return ["JOhn DOe", 33];
+};
+var user1 = getUser();
+console.log("name is ".concat(user1[0], ",  age is ").concat(user1[1]));
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,53 @@
-const x : number = 10
+// numbers like hex, decimal, binary, octal are all : number 
+const decimal: number = 0.7
+const hexNum : number = 0xf00f
+const age : number = 10
+// string is of type string 
+const userName : string = "John"
+// bools ar of type boolean
+const isEmployed: boolean = false
+const isStudent : boolean = true
 
-console.log(x);
+// undefined 
+const user: undefined = undefined
+const  n: null = null
+
+// arrays differ depending on what values they  include 
+const fruits: Array <string> = ["banana", "strawberry", "apple"]
+const fruits2: string[] = ["banana", "strawberry", "apple"]
+let arr: any [] = [34, 'hello', {name:"john"},[1,2,3]]
+arr[1] = "Hello World!"
+console.log(arr)
+const years : number[] = [2012, 2022, 2032]
+
+// mixed type arr
+let myMixedArr : (string | number)[] = ["33", 99]
+
+// array methods
+let arr2 : number[] = [22,44,24,33,66,77,95853]
+
+let evenNumbers: number []=  arr2.filter(n => n%2 === 0)
+let oddNums: number [] = arr2.filter(n => n%2 !== 0)
+console.log(`even numbers: ${evenNumbers}, odd  numbers are : ${oddNums}`);
+// tuples  are like arrays with differrent fixed types  having their indices fixed  
+let  abc :[number , string ]= [6, 'hello']
+const b: [boolean, string , number] = [ true, "hello",77]
+// can also have type inference , so you it is inferred 
+const myTuple = ["Macos", 2022, true, "Ubuntu", false]
+// deconstruct a tuple 
+let coordinates: [number, number] = [10, 20];
+let [x, y] = coordinates;
+
+console.log(x); // 10
+console.log(y); // 20
+
+// Tuples are commonly used to represent the parameters and return types of functions:
+
+const getUser =(): [string , number]=>{
+   return ["JOhn DOe", 33]
+}
+
+const user1: [string, number] =getUser()
+
+
+console.log(`name is ${user1[0]},  age is ${user1[1]}`);
